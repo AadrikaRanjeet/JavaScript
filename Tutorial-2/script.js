71dcff323be1fd6a8bfbf5645447ae0843bc5e81
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2024-07-31 11:00:24 +0530

    F

---
+++ Tutorial-2/script.js
@@ -0,0 +1,19 @@
+console.log("Hey this is module 2");
+
+
+// var a=5;
+// var b=6;
+// var c="Harry";
+// console.log(a+b+9+c);
+// console.log(typeof c);
+
+
+//dont use var , in modern Js use let 
+
+let a=89;
+
+{
+    let a=9;//block level variable jiska scope sirf block tk rhega
+    console.log(a);
+}
+console.log(a);
