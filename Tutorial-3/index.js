01c4c43cca20c99824019e4d1c75eac841bf3aaa
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2024-07-31 11:46:19 +0530

    G

---
+++ Tutorial-3/index.js
@@ -0,0 +1,53 @@
+//CONDITIONALS: IF ELSE , ELSE LADDER
+
+
+console.log("Heyyyyy");
+let age=18;
+if(age>18)
+{
+    console.log("can drive");
+}
+else
+{
+    console.log("cannot drive");
+}
+
+//operator
+
+let a=10;
+let b=2;
+
+console.log(a +b);
+console.log(a -b);
+console.log(a /b);
+console.log(a *b);
+console.log(a ** b);//exponential
+console.log(a %b);//remainder
+
+
+//Assignment Operator
+// =,+=,-=,8=,/=,%=,**=
+
+
+//Comparison Operator
+/* == (equl to) , !=(not equal) , ===(equal value and type) , !== (not equal value or not type)
+ > (greater than)
+ < (less than)
+ >=(greater than or equal to)
+ <=
+ ? (ternary operator)
+*/
+
+//Logical operator 
+/* 
+ && , || , !
+*/
+
+
+//ternary ooperator
+
+let m=10;
+let n=8;
+
+let c=a>b?(m-n):(n-m);
+console.log(c)
