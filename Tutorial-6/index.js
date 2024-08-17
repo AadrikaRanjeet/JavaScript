d04b08d6f72bc62f8c1c1b5604aaba128c3b74a0
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2024-08-17 09:55:28 +0530

    F

---
+++ Tutorial-6/index.js
@@ -0,0 +1,8 @@
+//Create a Faulty Calculator using JavaScript
+
+let random=Math.random();
+let a=prompt("Enter 1st number");
+let b=prompt("Enter 2nd number");
+let c=prompt("Enter operation");
+
+
