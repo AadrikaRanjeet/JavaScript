40cc8e10c2bd579485597c99cb18f92d66e85e20
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2024-08-17 10:30:22 +0530

    F

--- Tutorial-7/index.js
+++ Tutorial-7/index.js
@@ -19,3 +19,19 @@ console.log(name +" "+ secondname);//string concatenation
 //with the help of template literal u can use double qoute inside the sentence 
 console.log(`My name "is" ${name} ${secondname}`);//using template literal  
 
+
+//String Slicing
+console.log(name.slice(2,4)) //will print char from 2-4 where 4 is not included
+console.log(name.slice(1));//print char excluding 1st 
+
+
+//replace
+console.log(name.replace("Aa","af"));
+console.log(name.replace("Aadrika","Apoorva"));
+
+//concat
+console.log(name.concat(secondname));
+console.log(name.concat(secondname," is "));
+
+
+//Strings are immutable ie they remain unchanged
