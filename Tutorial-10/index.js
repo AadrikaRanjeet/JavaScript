b273d480b1e80cb0186439734e0f9b2d48049830
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2024-08-18 22:41:14 +0530

    G

--- Tutorial-10/index.js
+++ Tutorial-10/index.js
@@ -1,1 +1,6 @@
 console.log("Hello World")
+
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children //sirf agar elements chahiye ho toh
+document.body.firstElementChild.children[2].nextElementSibling.style.backgroundColor="blue"
